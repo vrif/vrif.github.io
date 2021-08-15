c3e055051a4713615ba5c32d301980a1ced4706e
Harry Yau <yauharry@gmail.com>
2021-08-14 23:50:09 -0700

    fix bootstrap installation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
